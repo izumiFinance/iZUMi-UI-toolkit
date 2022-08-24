5b093a12fb7cb7db20fea9a9f408119cff0f21ab
Abcold <841309023@qq.com>
2022-08-24 17:08:52 +0800

    update copyrightInfo translation

--- src/components/Sidebar/CopyrightInfo/CopyrightInfo.tsx
+++ src/components/Sidebar/CopyrightInfo/CopyrightInfo.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 
 const CopyrightInfo: React.FC = () => {
+    const { t } = useTranslation();
 
     return (
-        <Box
-            mb="66px"
-            fontSize="9px"
-            alignSelf="center"
-            fontFamily="Lato"
-            fontWeight="bold"
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="170"
-                height="10"
-                viewBox="0 0 170 10"
-            >
+        <Box mb="66px" fontSize="9px" alignSelf="center" fontFamily="Lato" fontWeight="bold">
+            <svg xmlns="http://www.w3.org/2000/svg" width="170" height="10" viewBox="0 0 170 10">
                 <text
                     id="_2021_izumi_Finance._All_rights_reserved."
                     data-name="@2021 izumi Finance. All rights reserved."
@@ -24,7 +15,7 @@ const CopyrightInfo: React.FC = () => {
                     x="-0.501"
                     y="7.375"
                 >
-                    @2021-2022 iZUMi. &nbsp; All rights reserved.
+                    @2021-2022 iZUMi. &nbsp; {t('All rights reserved.')}
                 </text>
             </svg>
         </Box>
