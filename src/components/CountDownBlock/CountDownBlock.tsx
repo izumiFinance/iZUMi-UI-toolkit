f08a30159ff2e7c61da79cdcf505d7147871ef1f
Abcold <841309023@qq.com>
2022-09-14 12:06:16 +0800

    update countdown in mobile

--- src/components/CountDownBlock/CountDownBlock.tsx
+++ src/components/CountDownBlock/CountDownBlock.tsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { i_text_copy_bold, i_text_d } from '../../style';
+import { i_text_copy, i_text_copy_bold, i_text_d } from '../../style';
 import { FiClock } from 'react-icons/fi';
-import { BoxProps, useColorMode, VStack, Text, Icon, HStack } from '@chakra-ui/react';
+import { BoxProps, useColorMode, VStack, Text, Icon, HStack, Image } from '@chakra-ui/react';
 import { getColorThemeSelector } from '../../utils/funcs';
 import useInterval from 'ahooks/lib/useInterval';
 import Card from '../Card/Card';
 import { useTranslation } from 'react-i18next';
+import { AdaptationMode } from '../layout/PageLayout';
 
 type BlockProps = {
     initialSeconds: number;
+    type?: AdaptationMode;
 } & BoxProps;
 
 const CountDownBlock: React.FC<BlockProps> = (props) => {
-    const { initialSeconds, ...rest } = props;
+    const { initialSeconds, type, ...rest } = props;
 
     const { t } = useTranslation();
     const colorTheme = getColorThemeSelector(useColorMode().colorMode);
@@ -33,7 +35,20 @@ const CountDownBlock: React.FC<BlockProps> = (props) => {
         setTimeLeft(timeLeft > 0 ? timeLeft - 1 : 0);
     }, 1000);
 
-    return (
+    return type === 'mobile' ? (
+        <HStack w="100%" alignItems="start" {...rest}>
+            <Text className={i_text_d} fontSize="12px !important" color="#A398BD">
+                {t('Current cycle')} :
+            </Text>
+            <HStack alignItems="center" px="10px" py="3px" spacing="10px" bg="#E0D4FB" borderRadius="14px">
+                <Image boxSize="16px" src="/assets/farm/cycle.svg" mt="2px"></Image>
+                <Text className={i_text_copy} color="#6F31FF" fontSize="12px !important">
+                    {days} {days > 1 ? t('days') : t('day')} {hours > 9 ? hours : `0${hours}`}:{minutes > 9 ? minutes : `0${minutes}`}:
+                    {seconds > 9 ? seconds : `0${seconds}`}
+                </Text>
+            </HStack>
+        </HStack>
+    ) : (
         <Card w="100%" h="50px" pl="10px" pt="4px" bg={colorTheme('#F5F5F5', 'tertiary.700')} {...rest}>
             <VStack alignItems="start" spacing="-2px">
                 <Text className={i_text_d} color={colorTheme('tertiary.600', 'tertiary.200')} fontSize="12px !important">
