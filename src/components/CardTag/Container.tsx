e62a2949c7d9cf4e612c97ff7c6ebd6655994e16
Abcold <841309023@qq.com>
2022-09-15 16:50:41 +0800

    update card container

--- src/components/CardTag/Container.tsx
+++ src/components/CardTag/Container.tsx
@@ -9,7 +9,7 @@ export const CardTagContainer: React.FC<props> = (props) => {
     const { children, ...rest } = props;
 
     return (
-        <HStack px="0px" position="absolute" top="0" left="32px" transform="translateY(-50%)" w="100%" {...rest}>
+        <HStack px="0px" position="absolute" top="0" left="32px" transform="translateY(-50%)" w={{ base: 'unset', sm: '100%' }} {...rest}>
             {children}
         </HStack>
     );
