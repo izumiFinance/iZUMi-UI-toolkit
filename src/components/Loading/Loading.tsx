b524b669d3f77efeaf6c13ebd9e455be75e480cd
McGrady00H <281130306@qq.com>
2022-10-19 17:42:33 +0800

    update

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -4,16 +4,16 @@ import { i_text_d } from '../../style';
 import { getColorThemeSelector } from '../../utils/funcs';
 
 type Props = {
-    //
+    text?: string
 } & BoxProps;
 
-export const Loading: React.FC<Props> = ({ ...rest }) => {
+export const Loading: React.FC<Props> = ({ text, ...rest }) => {
     const colorTheme = getColorThemeSelector(useColorMode().colorMode);
     return (
         <Flex direction="column" alignItems="center" justifyContent="center" pt="40px" {...rest}>
             <img width="20px" height="20px" src="/assets/gallery/loading.gif" />
             <Text className={i_text_d} color={colorTheme('tertiary.600', 'tertiary.400')} mt="16px" ml="12px">
-                Loading ...
+                {text?  text: 'Loading ...'}
             </Text>
         </Flex>
     );
