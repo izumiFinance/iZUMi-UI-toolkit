e934529144b2d2834a694b87e7ddbd5f8ae3ecdf
Abcold <841309023@qq.com>
2023-06-06 17:30:30 +0800

    update translate

--- src/components/ErrorInfo/ErrorInfo.tsx
+++ src/components/ErrorInfo/ErrorInfo.tsx
@@ -1,4 +1,5 @@
 import { BoxProps, HStack, Text, Image, useColorMode } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 import { i_text_copy_bold } from '../../style';
 import { getColorThemeSelector } from '../../utils/funcs';
 
@@ -8,6 +9,7 @@ type ErrorInfoProps = {
 export const ErrorInfo: React.FC<ErrorInfoProps> = (props) => {
     const { content, ...rest } = props;
     const colorTheme = getColorThemeSelector(useColorMode().colorMode);
+    const { t } = useTranslation();
 
     return (
         <HStack
@@ -20,7 +22,7 @@ export const ErrorInfo: React.FC<ErrorInfoProps> = (props) => {
         >
             <Image boxSize="11px" src="/assets/errorInfo/close.svg"></Image>
             <Text className={i_text_copy_bold} lineHeight="14px" letterSpacing="0.02em" color={'#FFFFFF'}>
-                {content}
+                {content && t(content)}
             </Text>
         </HStack>
     );
