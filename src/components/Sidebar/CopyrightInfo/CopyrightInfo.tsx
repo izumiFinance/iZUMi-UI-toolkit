51ac8634e9d549dc34830dd8948a7db78b9b7c54
AbcoldQAQ <lz841309023@gmail.com>
2024-03-13 15:07:59 +0800

    update copyright

--- src/components/Sidebar/CopyrightInfo/CopyrightInfo.tsx
+++ src/components/Sidebar/CopyrightInfo/CopyrightInfo.tsx
@@ -15,7 +15,7 @@ const CopyrightInfo: React.FC = () => {
                     x="-0.501"
                     y="7.375"
                 >
-                    @2021-2023 iZUMi. &nbsp; {t('All rights reserved.')}
+                    @2021-2024 iZUMi. &nbsp; {t('All rights reserved.')}
                 </text>
             </svg>
         </Box>
