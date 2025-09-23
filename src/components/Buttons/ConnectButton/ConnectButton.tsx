b7afb9ff7707cc493b319fe786ae6d88d1d5e3b1
AbcoldQAQ <lz841309023@gmail.com>
2025-09-23 16:36:08 +0800

    update translation

--- src/components/Buttons/ConnectButton/ConnectButton.tsx
+++ src/components/Buttons/ConnectButton/ConnectButton.tsx
@@ -4,7 +4,7 @@ import useIsMobile from '../../../hooks/useIsMobile';
 import { a_h5, i_text_d } from '../../../style';
 import { getColorThemeSelector } from '../../../utils/funcs';
 import CustomButton from '../CustomButton/CustomButton';
-
+import { useTranslation } from 'react-i18next';
 type ConnectButtonProps = {
     Identicon: any;
     onClick: () => void;
@@ -15,6 +15,7 @@ const ConnectButton: React.FC<ConnectButtonProps> = (props) => {
     const { Identicon, onClick, variant, ...rest } = props;
     const colorTheme = getColorThemeSelector(useColorMode().colorMode);
     const isMobile = useIsMobile();
+    const { t } = useTranslation();
     return variant && variant === 'black' ? (
         <CustomButton
             variant="none"
@@ -27,7 +28,7 @@ const ConnectButton: React.FC<ConnectButtonProps> = (props) => {
             text={
                 <Flex direction="row" align="center" justify="center">
                     <Text className={a_h5} as="span" fontFamily="Gilroy-Bold" color="tertiary.50">
-                        Connect to Wallet
+                        {t('Connect to Wallet')}
                     </Text>
                     <Image boxSize="23px" ml={{ base: '5px', sm: '12px' }} src={'/assets/wallet/walletRight.svg'}></Image>
                 </Flex>
@@ -47,7 +48,7 @@ const ConnectButton: React.FC<ConnectButtonProps> = (props) => {
             text={
                 <Flex direction="row" align="center" justify="center">
                     <Text className={i_text_d} as="span" fontSize="14px" fontWeight={600}>
-                        {isMobile ? 'Connect' : 'Connect Wallet'}
+                        {isMobile ? t('Connect') : t('Connect Wallet')}
                     </Text>
                     <Image boxSize="14px" src={'/assets/wallet/noWallet.svg'} ml="11px"></Image>
                 </Flex>
@@ -70,7 +71,7 @@ const ConnectButton: React.FC<ConnectButtonProps> = (props) => {
                         <Identicon size={18} />{' '}
                     </Box>
                     <Text className={i_text_d} as="span" fontSize="14px" fontWeight={600}>
-                        Connect Wallet
+                        {t('Connect Wallet')}
                     </Text>
                 </Flex>
             }
